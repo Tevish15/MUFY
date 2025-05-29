047543068de55331e85e4583427bb744a0d8fa9d
Tevish15 <tevish1503@gmail.com>
2025-05-29 16:05:02 +0000

    remove matplotlib

--- streamlit_chatbot/lovelanguage.py/game2.py
+++ streamlit_chatbot/lovelanguage.py/game2.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt  # Fix: import matplotlib for plotting
 
 # Page config
 st.set_page_config(page_title="Love Language Quiz", layout="centered")
